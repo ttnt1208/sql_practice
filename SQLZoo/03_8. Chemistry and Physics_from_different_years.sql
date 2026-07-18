0fc7bd8dd5a01fa86ad2f6343bc9bef989e8f181
ttnt1208 <tkhoctap2021@gmail.com>
2026-07-18 13:58:52 +0700

    xoa folder sqlzoo tren github

--- SQLZoo/03_8. Chemistry and Physics_from_different_years.sql
+++
@@ -1,9 +0,0 @@
--- Platform: w3resource
--- Problem: 8 
--- Difficulty: Easy
--- Link: https://sqlzoo.net/wiki/SELECT_from_Nobel_Tutorial
-
-select * 
-from nobel 
-where (subject = 'Physics' and yr =1980) or (subject = 'Chemistry' and yr = 1984); 
-
